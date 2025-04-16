05ff8fada0b3430d0d5271319eb583ce462d9eff
Nibedita Das <155538645+popeye04@users.noreply.github.com>
2025-04-16 11:56:51 -0700

    Create BankTransaction.h

---
+++ BankTransaction.h
@@ -0,0 +1,27 @@
+#ifndef BANKTRANSACTION_H
+#define BANKTRANSACTION_H
+
+#include <string>
+
+class BankTransaction
+{
+private:
+    int accountID;
+    char name[100], address[100], type[10];
+    float balance;
+    std::string password;
+
+public:
+    static int accountCount;
+    static BankTransaction accounts[100];
+
+    void OpenAccount();
+    void DepositMoney();
+    void WithdrawMoney();
+    void DisplayAmount();
+    int getAccountID();
+    bool verifyPassword();
+    static int SearchAccount(int id);
+};
+
+#endif // BANKTRANSACTION_H
