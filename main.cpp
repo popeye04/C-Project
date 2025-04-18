045a04631253d239b719b48a9efa6ef3f1ca430c
Nibedita Das <155538645+popeye04@users.noreply.github.com>
2025-04-18 11:22:15 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,21 +4,20 @@ using namespace std;
 
 int main()
 {
-
     int num, searchID, index;
     char option;
 
-    cout<<"\n||-------------------------------------------------||"<<endl;
-    cout<<"||--------------------ABC Bank---------------------||"<<endl;
-    cout<<"||------------------Welcome User!------------------||"<<endl;
-    cout<<"||-------------------------------------------------||"<<endl;
-    cout<<"\n1) Open Account."<<endl;
-    cout<<"2) Deposit Money."<<endl;
-    cout<<"3) Withdraw Money."<<endl;
-    cout<<"4) Display Account."<<endl;
-    cout<<"5) Open Admin Panel (Show All Accounts)." <<endl;
-    cout<<"6) Exit."<<endl;
-
+    cout << "\n||-------------------------------------------------||" << endl;
+    cout << "||--------------------ABC Bank---------------------||" << endl;
+    cout << "||------------------Welcome User!------------------||" << endl;
+    cout << "||-------------------------------------------------||" << endl;
+    cout << "\n1) Open Account." << endl;
+    cout << "2) Deposit Money." << endl;
+    cout << "3) Withdraw Money." << endl;
+    cout << "4) Display Account." << endl;
+    cout << "5) Open Admin Panel (Show All Accounts)." << endl;
+    cout << "6) Show Transaction History." << endl;
+    cout << "7) Exit." << endl;
 
     do
     {
@@ -28,12 +27,10 @@ int main()
         switch (num)
         {
         case 1:
-            cout << "\n1) Open Account:" << endl;
             BankTransaction::accounts[BankTransaction::accountCount].OpenAccount();
             break;
 
         case 2:
-            cout << "\n2) Deposit Money:" << endl;
             cout << "Enter Account ID: ";
             cin >> searchID;
             index = BankTransaction::SearchAccount(searchID);
@@ -48,7 +45,6 @@ int main()
             break;
 
         case 3:
-            cout << "\n3) Withdraw Money:" << endl;
             cout << "Enter Account ID: ";
             cin >> searchID;
             index = BankTransaction::SearchAccount(searchID);
@@ -63,7 +59,6 @@ int main()
             break;
 
         case 4:
-            cout << "\n4) Display Account:" << endl;
             cout << "Enter Account ID: ";
             cin >> searchID;
             index = BankTransaction::SearchAccount(searchID);
@@ -78,36 +73,42 @@ int main()
             break;
 
         case 5:
-            cout << "\n5) Admin Panel:"<<endl;
             BankTransaction::AdminView();
             break;
 
-
         case 6:
-            if (num == 6)
+            cout << "Enter Account ID: ";
+            cin >> searchID;
+            index = BankTransaction::SearchAccount(searchID);
+            if (index != -1 && BankTransaction::accounts[index].verifyPassword())
             {
-                cout<<"||---------------------Exited----------------------||"<<endl;
-                exit(0);
+                BankTransaction::accounts[index].ShowTransactionHistory();
             }
+            else
+            {
+                cout << "Invalid account or password!\n";
+            }
+            break;
 
+        case 7:
+            cout << "||---------------------Exited----------------------||" << endl;
+            exit(0);
 
         default:
-            cout<<"\nTry Again and Choose the Correct Option !"<<endl;
+            cout << "\nTry Again and Choose the Correct Option !" << endl;
             break;
         }
 
-        cout<<"\nDo You Want to Continue Banking Without Exit? Press(Y/N)."<<endl;
-        cin>>option;
+        cout << "\nDo You Want to Continue Banking Without Exit? Press(Y/N)." << endl;
+        cin >> option;
 
         if ((option == 'n') || (option == 'N'))
         {
-            cout<<"||---------------------Exited----------------------||"<<endl;
+            cout << "||---------------------Exited----------------------||" << endl;
             exit(0);
         }
 
-    }
-    while ((option == 'y') || (option == 'Y'));
-
+    } while ((option == 'y') || (option == 'Y'));
 
     return 0;
 }
