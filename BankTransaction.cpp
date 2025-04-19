82c4a41780d9b044075404b30a1d9167457ff04f
Nibedita Das <155538645+popeye04@users.noreply.github.com>
2025-04-19 01:28:37 -0700

    Update BankTransaction.cpp

--- BankTransaction.cpp
+++ BankTransaction.cpp
@@ -18,27 +18,38 @@ int BankTransaction::getAccountID()
 bool BankTransaction::verifyPassword()
 {
     string inputPass;
-    cout << "Enter your password: ";
-    cin >> inputPass;
+    cout<<"Enter your password: ";
+    cin>>inputPass;
     return inputPass == password;
 }
 
 void BankTransaction::OpenAccount()
 {
-    cout << endl << "Enter Account ID (within 12 digits): ";
-    cin >> accountID;
+    cout<<endl<< "Enter Account ID (within 12 digits): ";
+    cin>> accountID;
     cin.ignore();
-    cout << endl << "Enter Your Full Name:" << endl;
+    cout<< endl << "Enter Your Full Name:" <<endl;
     cin.getline(name, 100);
-    cout << endl << "Enter Your Address:" << endl;
+    cout<< endl << "Enter Your Address:" <<endl;
     cin.getline(address, 100);
-    cout << endl << "Type of Account You Want to Open savings(s) or current(c):" << endl;
-    cin.getline(type, 10);
-    cout << endl << "Enter Your Deposit Amount:" << endl;
-    cin >> balance;
-    cout << endl << "Set Your Account Password: ";
-    cin >> password;
-    cout << endl << "Your Account is Created Successfully." << endl;
+    while (true)
+    {
+        cout<< endl << "Type of Account You Want to Open savings(s) or current(c):" << endl;
+        cin.getline(type, 10);
+        if (type[0] == 's' || type[0] == 'S' || type[0] == 'c' || type[0] == 'C')
+        {
+            break;
+        }
+        else
+        {
+            cout << "Incorrect Account Type. Please Try Again." << endl;
+        }
+    }
+    cout<< endl << "Enter Your Deposit Amount:" <<endl;
+    cin>> balance;
+    cout<< endl << "Set Your Account Password: ";
+    cin>> password;
+    cout<< endl << "Your Account is Created Successfully." <<endl;
 
     accountCount++;
     SaveToFile();
@@ -59,55 +70,55 @@ void BankTransaction::OpenAccount()
 
 void BankTransaction::DepositMoney()
 {
-    int deposit;
-    cout << endl << "Enter the Amount You Want to Deposit:" << endl;
-    cin >> deposit;
+    float deposit;
+    cout<< endl << "Enter the Amount You Want to Deposit:" <<endl;
+    cin>> deposit;
     balance += deposit;
-    cout << "\nYour Current Balance is: " << balance << endl;
+    cout<< "\nYour Current Balance is: " << balance <<endl;
 
     transactionHistory.push("Deposited " + to_string(deposit));
 }
 
 void BankTransaction::WithdrawMoney()
 {
-    int withdraw;
-    cout << endl << "Enter the Amount You Want to Withdraw:" << endl;
-    cin >> withdraw;
+    float withdraw;
+    cout<< endl << "Enter the Amount You Want to Withdraw:" <<endl;
+    cin>> withdraw;
     if (withdraw <= balance)
     {
         balance -= withdraw;
-        cout << "\nYour Remaining Balance is: " << balance << endl;
+        cout<< "\nYour Remaining Balance is: " << balance <<endl;
         transactionHistory.push("Withdrew " + to_string(withdraw));
     }
     else
     {
-        cout << "\nInsufficient balance!" << endl;
+        cout<< "\nInsufficient balance!" <<endl;
     }
 }
 
 void BankTransaction::DisplayAmount()
 {
-    cout << endl << "Your Name: " << name << endl;
-    cout << "Your Address: " << address << endl;
-    cout << endl << "Type of Account: " << type << endl;
-    cout << "Account ID: " << accountID << endl;
-    cout << "Current/Total Balance: " << balance << endl;
+    cout<< endl << "Your Name: " << name <<endl;
+    cout<< "Your Address: " << address <<endl;
+    cout<< endl << "Type of Account: " << type <<endl;
+    cout<< "Account ID: " << accountID <<endl;
+    cout<< "Current/Total Balance: " << balance <<endl;
 }
 
 void BankTransaction::ShowTransactionHistory()
 {
     if (transactionHistory.empty())
     {
-        cout << "No transactions yet." << endl;
+        cout<< "No transactions yet." <<endl;
         return;
     }
 
-    cout << "\nRecent Transactions:\n";
+    cout<< "\nRecent Transactions:\n";
     std::stack<std::string> tempStack = transactionHistory;
 
     while (!tempStack.empty())
     {
-        cout << tempStack.top() << endl;
+        cout<< tempStack.top() <<endl;
         tempStack.pop();
     }
 }
@@ -127,23 +138,23 @@ int BankTransaction::SearchAccount(int id)
 void BankTransaction::AdminView()
 {
     int adminPassword;
-    cout << "Enter Admin Password: ";
-    cin >> adminPassword;
+    cout<< "Enter Admin Password: ";
+    cin>> adminPassword;
 
     if (adminPassword == 420)
     {
-        cout << "\nWelcome Admin!\n";
-        cout << "\nList of All Accounts (Sorted by Account ID):\n";
+        cout<< "\nWelcome Admin!\n";
+        cout<< "\nList of All Accounts (Sorted by Account ID):\n";
 
         for (int i = 0; i < accountCount; i++)
         {
-            cout << "Account " << i + 1 << ": " << accounts[i].name
+            cout<< "Account " << i + 1 << ": " << accounts[i].name
                 << " (ID: " << accounts[i].accountID << ")\n";
         }
     }
     else
     {
-        cout << "Incorrect Admin Password!\n";
+        cout<< "Incorrect Admin Password!\n";
     }
 }
 
@@ -159,7 +170,7 @@ void BankTransaction::SaveToFile()
     }
     else
     {
-        cout << "Unable to open file for writing.\n";
+        cout<< "Unable to open file for writing.\n";
     }
 }
 
@@ -201,5 +212,5 @@ void BankTransaction::LoadFromFile()
     }
 
     file.close();
-    std::cout << accountCount << " account(s) loaded from file.\n";
+    std::cout<< accountCount << " account(s) loaded from file.\n";
 }
