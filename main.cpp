02ca81b6efc5437549c308e4c8bbdfdbfd7cd02d
Nibedita Das <155538645+popeye04@users.noreply.github.com>
2025-04-19 01:27:14 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,22 +7,22 @@ int main()
     int num, searchID, index;
     char option;
 
-    cout << "\n||-------------------------------------------------||" << endl;
-    cout << "||-------------------COOL Bank---------------------||" << endl;
-    cout << "||------------------Welcome User!------------------||" << endl;
-    cout << "||-------------------------------------------------||" << endl;
-    cout << "\n1) Open Account." << endl;
-    cout << "2) Deposit Money." << endl;
-    cout << "3) Withdraw Money." << endl;
-    cout << "4) Display Account." << endl;
-    cout << "5) Open Admin Panel (Show All Accounts)." << endl;
-    cout << "6) Show Transaction History." << endl;
-    cout << "7) Exit." << endl;
+    cout<< "\n||-------------------------------------------------||" << endl;
+    cout<< "||-------------------COO |Bank---------------------||" << endl;
+    cout<< "||------------------Welcome User!------------------||" << endl;
+    cout<< "||-------------------------------------------------||" << endl;
+    cout<< "\n1) Open Account." << endl;
+    cout<< "2) Deposit Money." << endl;
+    cout<< "3) Withdraw Money." << endl;
+    cout<< "4) Display Account." << endl;
+    cout<< "5) Open Admin Panel (Show All Accounts)." << endl;
+    cout<< "6) Show Transaction History." << endl;
+    cout<< "7) Exit." << endl;
 
     do
     {
-        cout << "\nEnter an Option: ";
-        cin >> num;
+        cout<< "\nEnter an Option: ";
+        cin>> num;
 
         switch (num)
         {
@@ -31,8 +31,8 @@ int main()
             break;
 
         case 2:
-            cout << "Enter Account ID: ";
-            cin >> searchID;
+            cout<< "Enter Account ID: ";
+            cin>> searchID;
             index = BankTransaction::SearchAccount(searchID);
             if (index != -1 && BankTransaction::accounts[index].verifyPassword())
             {
@@ -40,13 +40,13 @@ int main()
             }
             else
             {
-                cout << "Invalid account or password!\n";
+                cout<< "Invalid account or password!\n";
             }
             break;
 
         case 3:
-            cout << "Enter Account ID: ";
-            cin >> searchID;
+            cout<< "Enter Account ID: ";
+            cin>> searchID;
             index = BankTransaction::SearchAccount(searchID);
             if (index != -1 && BankTransaction::accounts[index].verifyPassword())
             {
@@ -54,13 +54,13 @@ int main()
             }
             else
             {
-                cout << "Invalid account or password!\n";
+                cout<< "Invalid account or password!\n";
             }
             break;
 
         case 4:
-            cout << "Enter Account ID: ";
-            cin >> searchID;
+            cout<< "Enter Account ID: ";
+            cin>> searchID;
             index = BankTransaction::SearchAccount(searchID);
             if (index != -1 && BankTransaction::accounts[index].verifyPassword())
             {
@@ -77,8 +77,8 @@ int main()
             break;
 
         case 6:
-            cout << "Enter Account ID: ";
-            cin >> searchID;
+            cout<< "Enter Account ID: ";
+            cin>> searchID;
             index = BankTransaction::SearchAccount(searchID);
             if (index != -1 && BankTransaction::accounts[index].verifyPassword())
             {
@@ -86,29 +86,37 @@ int main()
             }
             else
             {
-                cout << "Invalid account or password!\n";
+                cout<< "Invalid account or password!\n";
             }
             break;
 
         case 7:
-            cout << "||---------------------Exited----------------------||" << endl;
+            cout<< "||---------------------Exited----------------------||" <<endl;
             exit(0);
 
         default:
-            cout << "\nTry Again and Choose the Correct Option !" << endl;
+            cout<< "\nTry Again and Choose the Correct Option !" <<endl;
             break;
         }
 
-        cout << "\nDo You Want to Continue Banking Without Exit? Press(Y/N)." << endl;
-        cin >> option;
+        cout<< "\nDo You Want to Continue Banking Without Exit? Press(Y/N)." <<endl;
+        cin>> option;
+        cout<< "\n1) Open Account." << endl;
+        cout<< "2) Deposit Money." << endl;
+        cout<< "3) Withdraw Money." << endl;
+        cout<< "4) Display Account." << endl;
+        cout<< "5) Open Admin Panel (Show All Accounts)." << endl;
+        cout<< "6) Show Transaction History." << endl;
+        cout<< "7) Exit." << endl;
 
         if ((option == 'n') || (option == 'N'))
         {
-            cout << "||---------------------Exited----------------------||" << endl;
+            cout<< "||---------------------Exited----------------------||" <<endl;
             exit(0);
         }
 
-    } while ((option == 'y') || (option == 'Y'));
+    }
+    while ((option == 'y') || (option == 'Y'));
 
     return 0;
 }
